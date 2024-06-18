de38001337295ebf5f4458dc87714bbd71665c74
Ramon <ramonzacharias44@googlemail.com>
2024-06-18 11:00:20 +0200

    Add persistent histories and new client

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from langserve import RemoteRunnable
 
 remote_chain = RemoteRunnable("http://localhost:8000/chain/")
-
+#remote_chain = RemoteRunnable("http://localhost:8000/chain_with_history/")
 while True:
     usr_input = input("User: ")
     print(remote_chain.invoke({"input": usr_input},
